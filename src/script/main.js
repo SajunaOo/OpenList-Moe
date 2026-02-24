dd836ef6207bc315d98fa59f835f33cf6063c98c
朱茱 <Sajuna@qq.com>
2026-02-24 21:22:28 +0800

    ✨ feat(theme): 主题色跟随 (#55)
    * ✨ feat(theme): 主题色跟随
    
    * 🐛 fix(js): 优化可读性

--- src/script/main.js
+++ src/script/main.js
@@ -15,8 +15,26 @@
  */
 
 /** 全屏背景图加载完成淡入 */
-window.addEventListener('load', () => document.body.classList.add('loaded'));
+function OpenList_Loaded() {
+  document.body.classList.add('loaded');
+}
 
+window.addEventListener('load', OpenList_Loaded);
+
+/** 主题色设置 */
+function hexToRgb(hex) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return `${r} ${g} ${b}`;
+}
+
+document.documentElement.style.setProperty(
+  '--moe-color-theme',
+  hexToRgb(window.OPENLIST_CONFIG?.main_color)
+);
+
+/** 控制台输出 */
 console.log(
   '\n %c OpenList Moe %c {{MOE_VERSION_LOG}} ',
   'padding: 5px 0; border-radius: 3px 0 0 3px; color: #fff; background: #FF6699; font-weight: bold;',
